dbcdeee8a382db58a75a05607b45ec3bc49316ed
Kai Curry <kai@cape.io>
2016-03-01 11:57:33 -0600

    invalid state can be used to define errors

--- src/select.js
+++ src/select.js
@@ -7,12 +7,21 @@ import { getPrefix } from './utils'
 export function selectForm(state) {
   return state.form
 }
+export function getErrorVal(state, { pristine, validate }) {
+  if (pristine) return null
+  if (isFunction(validate)) {
+    const errorVal = validate(state.value)
+    if (errorVal) return errorVal
+  }
+  if (state.invalid[state.value]) return state.invalid[state.value]
+  return state.error
+}
 // Validate function should return a string or object
 // error: { message: String, suggestion: String, status: String }
-export function derivedState(state, { validate, initialValue }) {
+export function derivedState(state, { initialValue, validate }) {
   const initVal = state.initialValue || initialValue || defaultState.initialValue
   const pristine = state.value === initVal
-  const errorVal = !pristine && isFunction(validate) ? validate(state.value) : state.error
+  const errorVal = getErrorVal(state, { pristine, validate })
   let status = errorVal ? 'error' : null
   if (errorVal && errorVal.status) {
     status = errorVal.status
@@ -28,12 +37,14 @@ export function derivedState(state, { validate, initialValue }) {
     errorMessage: errorVal && errorVal.message ? errorVal.message : errorVal,
     hasError: !!errorVal,
     initialValue: initVal,
+    invalid: state.invalid[state.value] || null,
+    isValid,
     open: state.blur || state.focus,
     pristine,
     saved: !pristine && state.value === state.savedValue,
     status,
     suggestion: errorVal && errorVal.suggestion ? errorVal.suggestion : null,
-    isValid,
+    valid: state.valid[state.value] || null,
   })
 }
 export function getState(state, props) {
--- test/select.spec.js
+++ test/select.spec.js
@@ -1,6 +1,6 @@
 import test from 'tape'
 
-import { onChange } from '../src/actions'
+import { onChange, valid } from '../src/actions'
 import { getState } from '../src/select'
 import reducer from '../src/reducer'
 
@@ -15,10 +15,22 @@ test('getState', t => {
     errorMessage: 'Required',
     dirty: true,
     hasError: true,
+    isValid: false,
     pristine: false,
     status: 'error',
     value: '',
   })
   t.deepEqual(getState({ form: state }, props), dirty, 'empty value')
+  let state2 = reducer(state, onChange(null, 'kai@foo.x'))
+  state2 = reducer(state2, valid(null, { key: 'kai@foo.x', value: { message: 'invalid' } }))
+  const dirty2 = dirty.merge({
+    errorMessage: null,
+    hasError: false,
+    isValid: true,
+    status: 'success',
+    valid: { message: 'invalid' },
+    value: 'kai@foo.x',
+  })
+  t.deepEqual(getState({ form: state2 }, {}), dirty2, 'valid is set')
   t.end()
 })
